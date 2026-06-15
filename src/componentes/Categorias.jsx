a228c5fd699489793e6b95248c411ca7eb605a5f
Karina Desposito <kvdesposito@gmail.com>
2026-06-15 15:38:37 -0300

    mejoras en fotos, agregado video y css

--- src/componentes/Categorias.jsx
+++ src/componentes/Categorias.jsx
@@ -1,25 +1,24 @@
 import Contenedor from './Contenedor'
 import Card from '../componentesReuse/Cards'
-import ventanaImg from '../recursos/imagenes/zapatillasVentana.webp'
-import ventanaSimpleImg from '../recursos/imagenes/ventana.webp'
+import ventanaImg from '../recursos/imagenes/zapatillasMontania.webp'
+import saleImg from '../recursos/imagenes/sale.webp'
 import modeloLocalImg from '../recursos/imagenes/modeloLocal.webp'
-import brooklinImg from '../recursos/imagenes/brooklin.webp'
+import accesoriosImg from '../recursos/imagenes/accesorios.webp'
 
 const categorias = [
   { nombre: 'Calzado', imagen: ventanaImg, ancho: 987, alto: 1593 },
   { nombre: 'Ropa', imagen: modeloLocalImg, ancho: 960, alto: 1280 },
-  { nombre: 'Accesorios', imagen: brooklinImg, ancho: 308, alto: 403 },
-  { nombre: 'Sale', imagen: ventanaSimpleImg, ancho: 721, alto: 1600 },
+  { nombre: 'Accesorios', imagen: accesoriosImg, ancho: 308, alto: 403 },
+  { nombre: 'Sale', imagen: saleImg, ancho: 721, alto: 1600 },
 ]
 
 function Categorias() {
   return (
     <section className="seccion categorias">
       <Contenedor>
-        {/* <div className="categorias__encabezado">
-          <p className="texto-etiqueta">Compra por categoría</p>
-          <h1>Elegidos para empezar por donde quieras.</h1>
-        </div> */}
+        <div className="categorias__encabezado">
+        
+        </div>
 
         <div className="grilla categorias__grilla">
           {categorias.map((categoria) => (
