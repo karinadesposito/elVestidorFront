4cd6145083fc479889cd7016ca6ff29dde90ed3c
Karina Desposito <kvdesposito@gmail.com>
2026-06-28 19:49:18 -0300

    arreglo de css, agregado de modal promocion y calzado

--- src/componentesReuse/Cards.jsx
+++ src/componentesReuse/Cards.jsx
@@ -1,9 +1,15 @@
-function Card({ imagen, ancho, alto, nombre }) {
+function Card({ imagen, ancho, alto, nombre, onClick }) {
   return (
-    <article className="card">
+    <article
+      className="card"
+      onClick={onClick}
+      role={onClick ? 'button' : undefined}
+      tabIndex={onClick ? 0 : undefined}
+    >
       <div className="card__visual" aria-hidden="true">
         <img src={imagen} alt="" width={ancho} height={alto} />
       </div>
+
       <h3 className="card__nombre">{nombre}</h3>
     </article>
   )
